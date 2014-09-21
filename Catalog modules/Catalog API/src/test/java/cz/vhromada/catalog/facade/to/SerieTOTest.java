511f3f02131eba1ae04cdc60a851f95c2901538a
Vladimir Hromada <vhromada@gmail.com>
2014-09-21 09:51:02 +0200

    Object generator

--- Catalog modules/Catalog API/src/test/java/cz/vhromada/catalog/facade/to/SerieTOTest.java
+++ Catalog modules/Catalog API/src/test/java/cz/vhromada/catalog/facade/to/SerieTOTest.java
@@ -1,15 +1,25 @@
 package cz.vhromada.catalog.facade.to;
 
 import cz.vhromada.catalog.commons.CollectionUtils;
+import cz.vhromada.generator.ObjectGenerator;
 import cz.vhromada.test.DeepAsserts;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * A class represents test for class {@link SerieTO}.
  *
  * @author Vladimir Hromada
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:testGeneratorContext.xml")
 public class SerieTOTest {
+
+	@Autowired
+	private ObjectGenerator objectGenerator;
 
 	/** Test method for {@link SerieTO#getGenresAsString()}. */
 	@Test
@@ -17,30 +27,17 @@ public class SerieTOTest {
 		final SerieTO serie = new SerieTO();
 		DeepAsserts.assertEquals("", serie.getGenresAsString());
 
-		final GenreTO genre1 = createGenreTO("Genre1");
+		final GenreTO genre1 = objectGenerator.generate(GenreTO.class);
 		serie.setGenres(CollectionUtils.newList(genre1));
 		DeepAsserts.assertEquals(genre1.getName(), serie.getGenresAsString());
 
-		final GenreTO genre2 = createGenreTO("Genre2");
+		final GenreTO genre2 = objectGenerator.generate(GenreTO.class);
 		serie.setGenres(CollectionUtils.newList(genre1, genre2));
 		DeepAsserts.assertEquals(genre1.getName() + ", " + genre2.getName(), serie.getGenresAsString());
 
-		final GenreTO genre3 = createGenreTO("Genre3");
+		final GenreTO genre3 = objectGenerator.generate(GenreTO.class);
 		serie.setGenres(CollectionUtils.newList(genre1, genre2, genre3));
 		DeepAsserts.assertEquals(genre1.getName() + ", " + genre2.getName() + ", " + genre3.getName(), serie.getGenresAsString());
-	}
-
-	/**
-	 * Returns new TO for genre.
-	 *
-	 * @param name name
-	 * @return new TO for genre
-	 */
-	private GenreTO createGenreTO(String name) {
-		final GenreTO genre = new GenreTO();
-		genre.setName(name);
-
-		return genre;
 	}
 
 }
